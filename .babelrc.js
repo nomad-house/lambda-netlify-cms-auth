761ca7699b68ecc8039a47e73a93c26bc497a0a9
Matthew Keil <me@matthewkeil.com>
2021-05-09 19:59:44 -0400

    publishing

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ["@babel/preset-env"],
-  plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]]
+  plugins: [
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
 };
